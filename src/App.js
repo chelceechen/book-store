58d3e0de1fd188adc027a538ea038350b0b3c57d
chelceechen <fromcyx@gmail.com>
2022-06-26 16:59:31 -0400

    book-store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.scss";
+import React, { useState } from "react";
+import PopUpWindow from "./components/PopUpWindow";
+import DeleteBookWindow from "./components/DeleteBookWindow";
+import CardItem from "./components/CardItem";
+import { useSelector } from "react-redux";
 
 function App() {
+  const bookList = useSelector((state) => state.bookStore.bookList);
+
+  const [type, setType] = useState("");
+  const [currentBook, setCurrentBook] = useState({});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="base-header">
+        <h1 className="text-center">Book Store</h1>
+        <div
+          type="button"
+          className="btn btn-info"
+          data-toggle="modal"
+          data-target="#myModal"
+          onClick={() => {
+            setType("Add A Book");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add A Book
+        </div>
+      </div>
+      <div className="base-body">
+        <div className="row">
+          {bookList.map((book) => (
+            <CardItem
+              key={book.id}
+              type={type}
+              setType={setType}
+              book={book}
+              setCurrentBook={setCurrentBook}
+            />
+          ))}
+        </div>
+      </div>
+      <PopUpWindow currentBook={currentBook} type={type} />
+      <DeleteBookWindow currentBook={currentBook} />
     </div>
   );
 }
-
 export default App;
